62813fc57472b76b40ddd64fd76b9e725b2fc4df
Dmitriy <fadeev97-1997@yandex.ru>
2022-08-17 00:06:08 +0400

    added exception

--- main.py
+++ main.py
@@ -16,33 +16,18 @@ def start(message):
 
 @bot.message_handler()
 def handle_text(message):
-    # wordForTranslate = message.text
-    try:
-        wordForTranslate = message.text
-    except ValueError:
-        bot.send_message(message.chat.id, "Please enter a word")
-    else:
-        srcLang = '1033'
-        dstLang = '1049'
-        url = f"https://developers.lingvolive.com/api/v1/Minicard?text={wordForTranslate}&srcLang={srcLang}&dstLang={dstLang}"
-        headers = {"Authorization": f"Bearer {bearerToken}"}
-        response = requests.get(url, headers=headers)
-        translatedWord = response.json()["Translation"]["Translation"]
-        # print(response.json()["Translation"]["Translation"])
-        bot.send_message(message.chat.id, 'Перевод: ' + translatedWord)
+    if message.text:
+        try:
+            wordForTranslate = message.text
+            srcLang = '1033'
+            dstLang = '1049'
+            url = f"https://developers.lingvolive.com/api/v1/Minicard?text={wordForTranslate}&srcLang={srcLang}&dstLang={dstLang}"
+            headers = {"Authorization": f"Bearer {bearerToken}"}
+            response = requests.get(url, headers=headers)
+            translatedWord = response.json()["Translation"]["Translation"]
+            bot.send_message(message.chat.id, 'Перевод: ' + translatedWord)
+        except Exception as ex:
+            bot.send_message(message.chat.id, "I can't find the translation")
 
 
 bot.polling(none_stop=True)
-
-# urlAuthenticate = 'https://developers.lingvolive.com/api/v1.1/authenticate'
-# apiKey = 'OGVmNzk4NjYtMjBhNC00OTkxLWIyMTUtYjBlZThhYWQzNzNkOjViYTBjN2MxNmVkZjQ1OTZhN2EzMjEwZGE0ZmQ5OWRm'
-# headersAuthenticate = {"Authorization": f"Basic {apiKey}"}
-#
-# bearerToken = requests.post(urlAuthenticate, headers=headersAuthenticate).text
-# wordForTranslate = 'house'
-# srcLang = '1033'
-# dstLang = '1049'
-# url = f"https://developers.lingvolive.com/api/v1/Minicard?text={wordForTranslate}&srcLang={srcLang}&dstLang={dstLang}"
-# headers = {"Authorization": f"Bearer {bearerToken}"}
-# response = requests.get(url, headers=headers)
-# print(response.json()["Translation"]["Translation"])
